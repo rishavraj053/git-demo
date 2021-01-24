b7e98214b525e8bf40884f00877b9d7c7a68a7b8
p-ragini <70903312+p-ragini@users.noreply.github.com>
2021-01-24 10:29:17 +0530

    SpringBoot project uploaded
    ToDoApplication

---
+++ ToDoApplication/src/main/java/com/ToDo/ToDoApplication/ToDoApplication.java
@@ -0,0 +1,14 @@
+package com.ToDo.ToDoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+		System.out.println("Hello,Good Morning Buddy");
+	}
+
+}
